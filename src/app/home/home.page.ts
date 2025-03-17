07b4cfd0d512c6465f6c923ee62b7cd3885f91db
Gabriel Pimenta <gabrielPimentape@outlook.com>
2025-03-17 12:10:35 -0300

    salvar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+interface Task{
+  id:number;
+  title:string;
+  completed:boolean;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [FormsModule, IonicModule, CommonModule],
 })
+
 export class HomePage {
+    tasks: Task[] = [];
+    newTask: string = "";
+
+    addTask(){
+      if (this.newTask.trim()) {
+        const newTaskObj: Task = {
+          id:Date.now(),
+          title: this.newTask.trim(),
+          completed: false
+        };
+
+      this.tasks.push(newTaskObj);
+
+      this.newTask = "";
+
+      console.log(this.tasks);
+      
+      }
+    }
   constructor() {}
 }
